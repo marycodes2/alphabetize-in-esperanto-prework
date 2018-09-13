80b84cfbf1ae2388b39261586fd818feab24bebe
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-13 02:18:59 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ esperanton_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   esperanton_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esperanton_alphabet = esperanton_alphabet.split(",")
-  arr.sort_by {|word|}
+  arr.sort_by {|word| esperanton_alphabet}
 end
 
 puts alphabetize(array)
