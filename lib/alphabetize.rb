4af1cfce7a49da32d8588ca755a19842c4af78c5
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-13 02:28:37 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,12 @@ esperanton_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   esperanton_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esperanton_alphabet = esperanton_alphabet.split(",")
-  sorted_list = arr.sort_by do |word| word.esperanton_alphabet
+  sorted_list = arr.sort_by do |word|
+    word.split(' ').map do |letter|
+      esperanton_alphabet.index(letter)
+    end
+  end 
+end 
   return sorted_list
 end
 
